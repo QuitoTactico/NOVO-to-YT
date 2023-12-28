ec4ef37607194b39e0e90de6da767634cb7a227f
Esteban Vergara Giraldo <Teteban0917@gmail.com>
2023-12-27 23:32:28 -0500

    Finalmente funciona a la perfección XD

--- NOVO-to-YT V2.py
+++ NOVO-to-YT V2.py
@@ -14,7 +14,7 @@ dir = "ListaTusArchivos-Script-v4 - Musica.csv"
     M  `MN. M          MM    6W'   `Wb           VMMP         MM      
     M   `MM.M  mmmmm   MM    8M     M8 mmmmm      MM          MM      
     M     YMM          MM    YA.   ,A9            MM          MM      
-  .JML.    YM          `Mbmo  `Ybmd9'           .JMML.      .JMML   2.6'''    
+  .JML.    YM          `Mbmo  `Ybmd9'           .JMML.      .JMML   2.9'''    
                                                                     
 
 
@@ -44,20 +44,32 @@ def listarCanciones(dir : str) -> dict:
     return novos
 
 
-def sussy_baka(lista : list) -> None:
+def sussy_baka(lista : list, carpeta: str) -> None:
     for cancion in lista:    print(cancion)
     print(f'\n[ {len(lista)} canciones en total ]')
 
     sleep(5)
     imagen_click('Opera')
     stop = 0
+    excepciones = 0
+    excepciones_lista = []
 
     for num in range(len(lista)):
+        stop +=1
         print(f'{num+1}/{len(lista)} | {round(((num+1)/len(lista)) *100 , 2)}% | {lista[num]}')
 
         buscar_cancion(lista, num)
 
-        agregar_cancion()
+        if imagen_buscar('Exception_para_ti'): 
+            excepciones += 1
+            excepciones_lista.append({'numero':num, 'nombre':lista[num]})
+            continue
+
+        try:
+            agregar_cancion(carpeta)
+        except:
+            excepciones += 1
+            excepciones_lista.append({'numero':num, 'nombre':lista[num]})
 
         #stop each 10
         if stop == 10:
@@ -65,22 +77,53 @@ def sussy_baka(lista : list) -> None:
             revisar_cola()
             revisar_terminal()
 
-        stop +=1
-
     p.press('i')
     imagen_click('Visual')
-    imagen_mover('Terminal')
+    terminal()
+    print(f'Excepciones: {excepciones}')
+    for i in excepciones_lista:
+        print(f'{i['numero']}\t| {i['nombre']}')
 
-def agregar_cancion():
+def agregar_cancion(carpeta):
     ver_puntos()
-    sleep(0.7)
-    imagen_click('Points')
-    sleep(0.7)
     try:
+        sleep(2)
+        imagen_click('Points', minSearchTime=0.5)
+    except:
+        sleep(0.5)
+        revisar_terminal()
+        sleep(2)
+        ver_puntos()
+        sleep(2)
+        try:
+            imagen_click('Points', region=None, minSearchTime=2)
+        except:
+            return None
+    finally:
+        try:
+            sleep(1)
+            imagen_click('Agregar_a_lista')
+        except:
+            sleep(2)
+            imagen_click('Agregar_a_lista')  
+        finally:
+            try:
+                sleep(1.5)
+                imagen_click(f'Novo/{carpeta}', confidence=0.99)
+            except:
+                sleep(2)
+                imagen_click(f'Novo/{carpeta}', confidence=0.97)    
+            finally:
+                p.press('esc')
+
+    '''
+    try:
+        sleep(0.7)
         imagen_click('Menu_cola')
     except:
         sleep(1)
         imagen_click('Menu_cola_icon')
+    '''
 
 def buscar_cancion(lista, num):
     buscar()
@@ -106,7 +149,7 @@ def pegar() -> None:
 
 def revisar_terminal():
     imagen_click('Visual')
-    imagen_mover('Terminal')
+    terminal()
     sleep(5)
     imagen_click('Opera')
 
@@ -117,7 +160,9 @@ def revisar_cola():
     p.press('i')
     sleep(6)
         
-def ver_puntos():  p.moveTo(655, 320, 0.2)
+def ver_puntos():  p.moveTo(670, 325, 0.5)
+
+def terminal():     p.moveTo(690, 625, 0.2)
 
 def barra_busqueda():   p.click(630, 125, duration=0.2)
 
@@ -126,7 +171,7 @@ def barra_busqueda():   p.click(630, 125, duration=0.2)
 # True : Existe | False : No papu :'V
 def imagen_buscar(image : str) -> bool:
     try:
-        p.locateCenterOnScreen(f'images/Exception_{image}.PNG', confidence=0.9)
+        p.locateCenterOnScreen(f'images/{image}.PNG', confidence=0.9)
         return True
     except:
         return False #safe en casos de excepciones
@@ -135,18 +180,34 @@ def imagen_mover(image, confidence = 0.8, duration = 0.2):
     p.moveTo(p.locateCenterOnScreen(f'images/{image}.PNG', confidence=confidence), 
              duration=duration)
 
-def imagen_click(image, confidence = 0.8, duration = 0.2):
-    p.click(p.locateCenterOnScreen(f'images/{image}.PNG', confidence=confidence), 
+def imagen_click(image, confidence = 0.8, duration = 0.2, region = None, minSearchTime = None):
+    try:
+        p.click(
+            p.locateCenterOnScreen(f'images/{image}.PNG', 
+                                   confidence=confidence,
+                                   region=region,
+                                   minSearchTime=minSearchTime), 
+            duration=duration)
+    except:
+        p.click(
+            p.locateCenterOnScreen(f'images/{image}.PNG', 
+                                   confidence=0.75, 
+                                   region=region,
+                                   minSearchTime=2), 
              duration=duration)
+
 
 # ------------ ejecución ----------------
 
 def pos(): p.displayMousePosition()
 
-lista_de_prueba = {'-Rock-': ['La costa del silencio',
+#pos()
+
+lista_de_prueba = {'-Prueba-': ['La costa del silencio',
                               'Bruno Mars - Locked out of haven',
                               'Gorillaz - Feel Good Inc.',
-                              'System Of A Down - Toxicity', 
+                              'System Of A Down - Toxicity',
+                              'Portugues', 
                               'back in black', 'Red Hot Chili Peppers - Scar Tissue [Official Music Video]',
                               '[MV] 이달의 소녀_츄 (LOONA_Chuu) _Heart Attack_']}
 
@@ -158,15 +219,18 @@ if __name__ == '__main__':
         print('\n¿Qué lista quisieras?', list(novos.keys()), '-> ', sep='\n', end='')
 
         # escoger la lista de reproducción/carpeta a crear en yt
-        lista_actual = input()
-        if lista_actual == '0': break
-        if lista_actual.isnumeric():     lista_actual = f'-Novo {lista_actual}-'
-        if lista_actual[0] != '-':       lista_actual = f'-{lista_actual}-'
-        print(lista_actual)
+        carpeta = input()
+        if carpeta == '0': break
+        if carpeta.isnumeric():     carpeta = f'-Novo {carpeta}-'
+        if carpeta[0] != '-':       carpeta = f'-{carpeta}-'
+        print(carpeta)
         
-        when = 'Está en la lista, hora del sexo' if lista_actual in novos.keys() else 'Eres un imbécil'
+        when = 'Está en la lista, hora del sexo' if carpeta in novos.keys() else 'Eres un imbécil'
         print(when, '\n')
 
-        if when != 'Eres un imbécil': sussy_baka(novos[lista_actual])
+        if when != 'Eres un imbécil': 
+            desde = int(input('Desde dónde bro? -> '))
+            lista = novos[carpeta][desde:]
+            sussy_baka(lista, carpeta)
 
     print('La re buena, chao')
